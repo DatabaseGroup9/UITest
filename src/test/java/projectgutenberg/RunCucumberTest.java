cc9a25ebaedd94b6b1175eaf98ee325ced0e40a8
Cherry Rose Semena <cph-cs241@cphbusiness.dk>
2018-05-21 21:11:58 +0200

    added ui test for User Story 1

---
+++ src/test/java/projectgutenberg/RunCucumberTest.java
@@ -0,0 +1,10 @@
+package projectgutenberg;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty"})
+public class RunCucumberTest {
+}
